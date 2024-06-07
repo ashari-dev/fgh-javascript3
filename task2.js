08925466d79f9614737d25364f93357f747137e5
muhammad ashari <boykid923@gmail.com>
2024-06-07 18:05:34 +0700

    js3

--- task2.js
+++ task2.js
@@ -20,3 +20,14 @@ const getMonth = (callback) => {
     } else callback(new Error("sorry Data Not Found"), []);
   }, 4000);
 };
+
+function showMonth(err, arr) {
+  if (err) {
+    console.log(err.message);
+  } else {
+    const bulan = arr.map((value) => value);
+    console.log(bulan);
+  }
+}
+
+getMonth(showMonth);
